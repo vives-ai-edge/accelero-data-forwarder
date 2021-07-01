5c8eb2ed2421926e677c072a97342973c99ccaec
Sille Van Landschoot <info@sillevl.be>
2021-07-01 14:36:41 +0200

    add default console override for serial output

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "USBSerial.h"
 #include "LSM303AGRAccSensor.h"
 
-USBSerial ser;
+USBSerial serial;
 DigitalOut led((PinName)0x6C);
 
 volatile bool trig = 0;
@@ -12,6 +12,11 @@ int32_t axes[3];
 
 SPI devSPI(PB_15, NC, PB_13);  // 3-wires SPI on SensorTile  
 static LSM303AGRAccSensor accelerometer(&devSPI, PC_4);
+
+
+FileHandle *mbed::mbed_override_console(int) {
+    return &serial;
+}
 
 void sample(){
     trig=1;
@@ -23,16 +28,16 @@ int main(void)
     accelerometer.init(NULL);
     accelerometer.enable();
     accelerometer.read_id(&id);
-    ser.printf("LSM303AGR accelerometer           = 0x%X\r\n", id); 
+    printf("LSM303AGR accelerometer           = 0x%X\r\n", id); 
     accelerometer.get_x_axes(axes);
-    ser.printf("LSM303AGR [acc/mg]:      %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
+    printf("LSM303AGR [acc/mg]:      %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
     timer.attach_us(&sample,10100);
 
     while(true){
         while (!trig){}
         trig = 0;
         accelerometer.get_x_axes(axes);
-        ser.printf("%ld,%ld,%ld\n\r",axes[0],axes[1],axes[2]);
+        printf("%ld,%ld,%ld\n\r",axes[0],axes[1],axes[2]);
         led = !led;
     }
 }
